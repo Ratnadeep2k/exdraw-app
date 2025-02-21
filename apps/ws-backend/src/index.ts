d6b2333579a58a4c5574a72e617308284088ca76
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-21 19:16:34 +0530

    feat(ws-backend): implement JWT authentication for WebSocket connections

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,23 @@
 import {WebSocketServer} from 'ws';
-const wss = new WebSocketServer({port: 8080});
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
 
-wss.on('connection',function connection(ws){
+
+
+const wss = new WebSocketServer({port: 8080});
+wss.on('connection',function connection(ws ,request){
+    const url  = request.url; // ws://localhost:8080/token=123123
+    //[ws://localhost:8080,token = 123123]
+    if(!url){
+        return ;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token =queryParams.get('token') || "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return
+    }
     ws.on('message',function message(data){
         ws.send('something');
     });
